aa74b0a85a4398528af8e4f33f764e6e34ddd2a8
fang duan <df1228@gmail.com>
2022-03-23 15:28:49 +0800

    deregister when shutdown rpc service close #14

--- zrpc/registry/consul/register.go
+++ zrpc/registry/consul/register.go
@@ -73,17 +73,18 @@ func RegisterService(listenOn string, c Conf) error {
 	}()
 	// consul deregister
 	proc.AddShutdownListener(func() {
-		err := client.Agent().ServiceDeregister(serviceID)
-		if err != nil {
-			logx.Info("deregister service error: ", err.Error())
-		} else {
-			logx.Info("deregistered service from consul server.")
-		}
-		err = client.Agent().CheckDeregister(serviceID)
+		err := client.Agent().CheckDeregister(serviceID)
 		if err != nil {
 			logx.Info("deregister check error: ", err.Error())
 		}
-	})
+		logx.Info("deregistered check from consul server.")
+
+		err = client.Agent().ServiceDeregister(serviceID)
+		if err != nil {
+			logx.Info("deregister service error: ", err.Error())
+		}
+		logx.Info("deregistered service from consul server.")
+	})()
 
 	return nil
 }
